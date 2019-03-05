bd1b9d0b489815c93446eaaf955ca7846c7f74e9
Ian Walter <public@iankwalter.com>
2019-03-05 08:56:36 -0500

    Getting rid of grunt-simple-mocha
    And completing Aconex/drakov#186

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,15 @@
 module.exports = function (grunt) {
+  grunt.initConfig({
+    'blueprint-validator': {
+      'contract-test': {
+        mdFiles: 'test/example/**/*.md',
+        failOnWarnings: true
+      }
+    }
+  })
 
-    grunt.initConfig({
+  grunt.loadNpmTasks('grunt-blueprint-validator')
 
-        simplemocha: {
-            options: {
-                globals: ['should'],
-                timeout: 3000,
-                ignoreLeaks: false,
-                ui: 'bdd',
-                reporter: 'tap'
-            },
-            api: {src: 'test/api/*-test.js'},
-            unit: {src: 'test/unit/*-test.js'}
-        },
-        'blueprint-validator': {
-            'contract-test':{
-                mdFiles: 'test/example/**/*.md',
-                failOnWarnings: true
-            }
-        }
-    });
-
-    // For this to work, you need to have run `npm install grunt-simple-mocha`
-    grunt.loadNpmTasks('grunt-simple-mocha');
-    grunt.loadNpmTasks('grunt-blueprint-validator');
-
-    grunt.task.registerTask('unit-test', ['simplemocha:unit']);
-    grunt.task.registerTask('test', ['blueprint-validator', 'simplemocha:api', 'simplemocha:unit']);
-    grunt.task.registerTask('default', ['test']);
-};
+  grunt.task.registerTask('test', ['blueprint-validator'])
+  grunt.task.registerTask('default', ['test'])
+}
