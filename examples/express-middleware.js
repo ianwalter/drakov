7dc5ed6f989e821e8a13efc9f7fbbde77cad16fc
Ian Walter <public@iankwalter.com>
2019-03-29 09:58:47 -0400

    Upgrading eslint-config

--- examples/express-middleware.js
+++ examples/express-middleware.js
@@ -5,13 +5,15 @@ const drakovMiddleware = require('../index.js').middleware
 // const drakov = require('drakov')
 
 const app = express()
-app.use(requestUtils.getBody) // need to have a body parser for the middleware to work
+app.use(requestUtils.getBody) // need to have a body parser for the middleware
+// to work
 
 const drakovOptions = {
   sourceFiles: '../test/example/**/*.md'
 }
 
-// currently need to initialise the middleware asynchronously due to drafter parse in async
+// currently need to initialise the middleware asynchronously due to drafter
+// parse in async
 drakovMiddleware.init(app, drakovOptions, function (err, middlewareFunction) {
   if (err) {
     throw err
